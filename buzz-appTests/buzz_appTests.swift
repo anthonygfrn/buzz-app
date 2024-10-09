da5f21c0f158fe9571258187e3f01040ef8cd7ee
Kurniakun17 <kurniakharisma17@gmail.com>
2024-10-09 09:32:12 +0700

    chore: change the build settings in project.yml

--- buzz-appTests/buzz_appTests.swift
+++ buzz-appTests/buzz_appTests.swift
@@ -4,26 +4,24 @@
 //
 //  Created by Anthony on 07/10/24.
 //
+
+@testable import buzz_app
+import Testing
+
+struct buzz_appTests {
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+}
+
+// import XCTest
+// @testable import buzz_app
 //
-//import Testing
-//@testable import buzz_app
+// final class buzz_appTests: XCTestCase {
 //
-//struct buzz_appTests {
-//
-//    @Test func example() async throws {
-//        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+//    func testExample() async throws {
+//        // Write your test here
+//        XCTAssertEqual(1, 1) // An example test assertion
 //    }
 //
-//}
-
-import XCTest
-@testable import buzz_app
-
-final class buzz_appTests: XCTestCase {
-
-    func testExample() async throws {
-        // Write your test here
-        XCTAssertEqual(1, 1) // An example test assertion
-    }
-
-}
+// }
