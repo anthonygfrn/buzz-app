7b34aa9f9aa57bd3c9b9461c496de5039b0b9103
Anthony Gufron <81062477+anthonygfrn@users.noreply.github.com>
2024-10-17 07:45:16 +0700

    feat: toolbar & parent buttons

---
+++ buzz-app/Presentation/Views/Shared/CustomToolbar.swift
@@ -0,0 +1,18 @@
+//
+//  CustomToolbar.swift
+//  buzz-app
+//
+//  Created by Anthony on 16/10/24.
+//
+
+import SwiftUI
+
+struct CustomToolbar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomToolbar()
+}
---
+++ buzz-app/Presentation/Views/Shared/ToolbarButton.swift
@@ -0,0 +1,18 @@
+//
+//  ToolbarButton.swift
+//  buzz-app
+//
+//  Created by Anthony on 16/10/24.
+//
+
+import SwiftUI
+
+struct ToolbarButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToolbarButton()
+}
