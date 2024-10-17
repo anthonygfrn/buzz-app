7fbeab5be328a6f8d13c009c023ab413cd9d66f9
Anthony Gufron <81062477+anthonygfrn@users.noreply.github.com>
2024-10-17 09:46:35 +0700

    Fix: Styling

---
+++ buzz-app/Presentation/Views/Shared/FontPicker.swift
@@ -0,0 +1,18 @@
+//
+//  FontPicker.swift
+//  buzz-app
+//
+//  Created by Anthony on 17/10/24.
+//
+
+import SwiftUI
+
+struct FontPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FontPicker()
+}
