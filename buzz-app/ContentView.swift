1b8174b8173077cb2ed5a8b4c09216c07a35eb7b
Rangga <rangga.yudhistira@ui.ac.id>
2024-11-04 11:41:26 +0700

    Remove top and bottom padding

--- buzz-app/ContentView.swift
+++ buzz-app/ContentView.swift
@@ -8,47 +8,44 @@ struct ContentView: View {
         applyColorModeUseCase: ApplyColorModeUseCase(),
         applyFontAttributesUseCase: ApplyFontAttributesUseCase()
     )
-
+    
     @StateObject var darkModeViewModel = DarkModeViewModel()
-
+    
     var body: some View {
-        VStack {
+        VStack(spacing: 0) { // Mengatur spacing menjadi 0 untuk menghilangkan ruang kosong
             GeometryReader { geometry in
                 ScrollView {
                     VStack {
-                        // Calculate padding based on the screen width and given ratio
                         let totalWidth = geometry.size.width
                         let totalHeight = geometry.size.height
-
+                        
                         let contentWidth: CGFloat = min(totalWidth * 0.80, 1424)
                         let sidePadding = (totalWidth - contentWidth) / 2
-
-                        // Center the RichTextEditor with calculated padding
+                        
                         RichTextEditor(text: $viewModel.extractedText, context: viewModel.context)
-                            .frame(width: contentWidth, height: totalHeight - 106)
-                            .padding(.top, 106)
+                            .frame(width: contentWidth, height: totalHeight)
                             .padding(.leading, sidePadding)
                             .padding(.trailing, sidePadding)
+                            
                     }
+                    .frame(maxWidth: .infinity)
                 }
+                .background(Color("BgColor"))
             }
-
             CustomToolbar()
         }
         .environmentObject(viewModel)
-        .background(Color("BgColor"))
         .onAppear {
-            openPDFPicker() // Call the PDF picker when the view appears
+            openPDFPicker()
         }
     }
-
-    // Function to open PDF from Finder (macOS)
+    
     func openPDFPicker() {
         let panel = NSOpenPanel()
         panel.allowedFileTypes = ["pdf"]
         panel.allowsMultipleSelection = false
         panel.canChooseDirectories = false
-
+        
         if panel.runModal() == .OK {
             if let url = panel.url {
                 viewModel.openPDF(url: url)
